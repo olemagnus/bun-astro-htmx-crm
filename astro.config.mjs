58db738126a8fd54ff4e800b2795540f165e8fcc
Ole Magnus Støvern <oles@hey.com>
2023-10-11 16:00:42 +0200

    adds basic htmx example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
 });
